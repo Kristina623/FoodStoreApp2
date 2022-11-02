7b4b36a748983c34556c7ea13877ee2c8cf4d55b
Pavel <62638053+PavelAndreevichG@users.noreply.github.com>
2022-11-02 10:00:52 +0300

    Добавьте файлы проекта.

---
+++ FoodStoreApp/Controllers/ManufacturersController.cs
@@ -0,0 +1,67 @@
+﻿using FoodStoreApp.Data;
+using FoodStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodStoreApp.Controllers
+{
+    public class ManufacturersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ManufacturersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Manufacturers> manufacturerList = _db.Manufacturers;
+            return View(manufacturerList);
+        }
+       
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Manufacturers cat)
+        {
+
+            _db.Manufacturers.Add(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            return View();
+        }
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var cat = _db.Manufacturers.Find(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            _db.Manufacturers.Remove(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FoodStoreApp/Models/Manufacturers.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodStoreApp.Models
+{
+    public class Manufacturers
+    {
+        public int Id { get; set; }
+        [DisplayName(" Производитель")]
+        public string Name { get; set; }
+        [DisplayName(" Страна")]
+        public string Country { get; set; }
+    }
+}
+
